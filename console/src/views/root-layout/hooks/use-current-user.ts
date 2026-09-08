17893cec35e17763d762dabdac6caf0926a6b5fb
Prasanna Ramesh <44441571+Prasanna-ramesh@users.noreply.github.com>
2026-09-08 07:04:21 +0200

    chore: move to bun

--- console/src/views/root-layout/hooks/use-current-user.ts
+++ console/src/views/root-layout/hooks/use-current-user.ts
@@ -7,6 +7,8 @@ export const useCurrentUser = () => {
         queryFn: () => fetchClient.GET('/api/v1/users'),
     });
 
+    console.log({ isPending, data, isError, error });
+
     return {
         isPending,
         isError,
